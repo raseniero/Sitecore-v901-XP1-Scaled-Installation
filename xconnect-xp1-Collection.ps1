b5748fae9ca4e13bff4ec02ac7a7b1d885837ec9
Ramon Aseniero <raseniero@gmail.com>
2018-06-27 23:10:56 -0700

    fix params

--- xconnect-xp1-Collection.ps1
+++ xconnect-xp1-Collection.ps1
@@ -1,7 +1,10 @@
 #define parameters 
 $Prefix = "xp" 
 $PSScriptRoot = "c:\xp"
+
 $ConfigPath = "$PSScriptRoot\config"
+$CertificateName = "$Prefix.xconnect_client" 
+$SitecoreSiteName = "$Prefix.sc" 
 
 $SolrUrl = "https://localhost:8983/solr" 
 $SolrRoot = "C:\solr-6.6.2" 
@@ -12,20 +15,20 @@ $SqlAdminUser = "sa"
 $SqlAdminPassword="jajnav5@" 
   
 #install sitecore instance 
-$xconnectHostName = "$prefix.xconnect" 
-$sitecoreParams = @{     
+$XConnectHostName = "$Prefix.xconnect" 
+$SitecoreParams = @{     
     Path = "$ConfigPath\xconnect-xp1-collection.json"     
     Package = "$PSScriptRoot\Sitecore 9.0.1 rev. 171219 (OnPrem)_xp1collection.scwdp.zip"     
     LicenseFile = "$PSScriptRoot\license.xml"     
-    SqlDbPrefix = $prefix  
+    SqlDbPrefix = $Prefix  
     SqlServer = $SqlServer  
     SqlAdminUser = $SqlAdminUser     
     SqlAdminPassword = $SqlAdminPassword     
-    SolrCorePrefix = $prefix  
+    SolrCorePrefix = $Prefix  
     SolrUrl = $SolrUrl     
-    XConnectCert = $certParams.CertificateName     
-    Sitename = $sitecoreSiteName         
+    XConnectCert = $CertificateName     
+    Sitename = $SitecoreSiteName         
     XConnectCollectionService = "https://$XConnectCollectionService"    
 } 
-Write-Host @solrParams
-Install-SitecoreConfiguration @sitecoreParams 
+Write-Host @SitecoreParams
+Install-SitecoreConfiguration @SitecoreParams 
