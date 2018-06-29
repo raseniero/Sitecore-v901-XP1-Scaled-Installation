7fce3bd7c1066d636e7a4c83d336438811c1b4c9
Ramon Aseniero <raseniero@gmail.com>
2018-06-29 11:29:06 -0700

    dev complete xconnect collection services, added copyrights

--- xconnect-xp1-Collection.ps1
+++ xconnect-xp1-Collection.ps1
@@ -1,34 +1,79 @@
-#define parameters 
-$Prefix = "xp" 
+<#
+Exposed parameters for creating the Collection Service, so you can change it for production
+#>
+$Prefix = "xp901" 
 $PSScriptRoot = "c:\xp"
 
-$ConfigPath = "$PSScriptRoot\config"
-$CertificateName = "$Prefix.xconnect_client" 
-$SitecoreSiteName = "$Prefix.sc" 
+$Path = "$PSScriptRoot\config\xconnect-xp1-collection.json"
+$Package = "$PSScriptRoot\Sitecore 9.0.1 rev. 171219 (OnPrem)_xp1collection.scwdp.zip"
+$LicenseFile = "$PSScriptRoot\license.xml"
+$SiteName = "$Prefix.sc" 
 
-$SolrUrl = "https://localhost:8983/solr" 
-$SolrRoot = "C:\solr-6.6.2" 
-$SolrService = "solr662" 
+$SSLCert = "" #Todo: needs to be provided, if not then generated
+$XConnectCert = "$Prefix.xconnect_client"
 
-$SqlServer = "RAMONASENIE0E1F" 
+$SqlDbPrefix = $Prefix
 $SqlAdminUser = "sa" 
 $SqlAdminPassword="jajnav5@" 
-  
+$SqlCollectionUser = "collectionuser"
+$SqlCollectionPassword = "Test12345"
+$SqlProcessingPoolsUser = "poolsuser"
+$SqlProcessingPoolsPassword = "Test12345"
+$SqlMarketingAutomationUser = "marketingautomationuser"
+$SqlMarketingAutomationPassword = "Test12345"
+$SqlMessagingUser = "messaginguser"
+$SqlMessagingPassword = "Test12345"
+$SqlServer = "RAMONASENIE0E1F" 
+
+$XConnectEnvironment ="Development" #For production environment use Production
+$XConnectLogLevel = "Information" #Can be Debug
+
 #install sitecore instance 
-$XConnectHostName = "$Prefix.xconnect" 
-$SitecoreParams = @{     
-    Path = "$ConfigPath\xconnect-xp1-collection.json"     
-    Package = "$PSScriptRoot\Sitecore 9.0.1 rev. 171219 (OnPrem)_xp1collection.scwdp.zip"     
-    LicenseFile = "$PSScriptRoot\license.xml"     
-    SqlDbPrefix = $Prefix  
-    SqlServer = $SqlServer  
+$XConnectCollectionParams = @{     
+    Path = $Path  
+    Package = $Package     
+    LicenseFile = $LicenseFile
+    SiteName = $SiteName
+    SSLCert = $SSLCert
+    XConnectCert = $XConnectCert
+    SqlDbPrefix = $SqlDbPrefix  
     SqlAdminUser = $SqlAdminUser     
-    SqlAdminPassword = $SqlAdminPassword     
-    SolrCorePrefix = $Prefix  
-    SolrUrl = $SolrUrl     
-    XConnectCert = $CertificateName     
-    Sitename = $SitecoreSiteName         
-    XConnectCollectionService = "https://$XConnectCollectionService"    
+    SqlAdminPassword = $SqlAdminPassword
+    SqlCollectionUser = $SqlCollectionUser
+    SqlCollectionPassword = $SqlCollectionPassword
+    SqlProcessingPoolsUser = $SqlProcessingPoolsUser
+    SqlProcessingPoolsPassword = $SqlProcessingPoolsPassword
+    SqlMarketingAutomationUser = $SqlMarketingAutomationUser
+    SqlMarketingAutomationPassword = $SqlMarketingAutomationPassword
+    SqlMessagingUser = $SqlMessagingUser
+    SqlMessagingPassword = $SqlMessagingPassword
+    SqlServer = $SqlServer 
+    XConnectEnvironment = $XConnectEnvironment
+    XConnectLogLevel = $XConnectLogLevel             
 } 
-Write-Host @SitecoreParams
-Install-SitecoreConfiguration @SitecoreParams 
+Write-Host @XConnectCollectionParams
+Install-SitecoreConfiguration @XConnectCollectionParams -Verbose
+
+<#
+MIT License
+
+Copyright (c) 2018 Ramon Aseniero
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
